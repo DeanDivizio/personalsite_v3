64a5fedc0cec9caefb6501e503e2d72f51d34827
Dean Divizio <dean@deandivizio.com>
2024-05-03 16:53:13 -0400

    conditionally animating media section elements based on client device

--- app/page.js
+++ app/page.js
@@ -50,6 +50,7 @@ export default function Home() {
   let startOffset = 0.25;
 
   const [loveStyle, setLoveStyle] = useState({ color: '#ffffff', fontWeight: '400' }); // state for the style of the word "Love" in the "I Love Learning" section. used to enable the fade to gradient effect
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 645); // state for detecting screen width. used to determine if the media section should animate in
 
   const controls = useAnimation(); // animation controls for the images in the media section
   const heroControls = useAnimation(); // animation controls for hero section
@@ -184,6 +185,12 @@ export default function Home() {
     else { controls.start("exit") }
   }, [controls, inView]);
 
+  useEffect(() => { // handles state changes for the isDesktop state during window resizing
+    const handleResize = () => setIsDesktop(window.innerWidth > 645);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   // States used for toggling between meddis sub-sections. there are three because the event is split into multiple stages that need to animate in sequence
   const [isToggled1, setToggle1] = useState(false);
   const [isToggled2, setToggle2] = useState(false);
@@ -266,47 +273,47 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div ref={mediaRef}>
-          <motion.div className={styles.headingSection} animate={mediaControls} variants={fromBottomVariants} initial="hidden" custom={0}>
-            <h2>I'm a</h2>
-            <div className={styles.toggleContainer} onClick={handleClick}>
-              <div className={styles.label} style={{ color: isToggled1 ? '#000000' : '#0073ff', fontWeight: isToggled1 ? '200' : '200', opacity: isToggled1 ? '0.65' : '1' }}>Photo</div>
-              <div className={`${styles.toggleButton} ${isToggled1 ? styles.active : ''}`}></div>
-              <div className={styles.label} style={{ color: isToggled1 ? '#0073ff' : '#000000', fontWeight: isToggled1 ? '200' : '200', opacity: isToggled1 ? '1' : '0.65' }}>Video</div>
+          <div ref={mediaRef}> {/* below components are animated in conditionally if not on mobile */}
+            <motion.div className={styles.headingSection} animate={isDesktop ? mediaControls : undefined} variants={isDesktop ? fromBottomVariants : undefined} initial={isDesktop ? "hidden" : undefined} custom={0}>
+              <h2>I'm a</h2>
+              <div className={styles.toggleContainer} onClick={handleClick}>
+                <div className={styles.label} style={{ color: isToggled1 ? '#000000' : '#0073ff', fontWeight: isToggled1 ? '200' : '200', opacity: isToggled1 ? '0.65' : '1' }}>Photo</div>
+                <div className={`${styles.toggleButton} ${isToggled1 ? styles.active : ''}`}></div>
+                <div className={styles.label} style={{ color: isToggled1 ? '#0073ff' : '#000000', fontWeight: isToggled1 ? '200' : '200', opacity: isToggled1 ? '1' : '0.65' }}>Video</div>
+              </div>
+              <h2>Pro</h2>
+            </motion.div>
+            <div className={styles.mediaContainer}>
+              {!isToggled3 &&
+                <div className={styles.photoSection} style={{ opacity: isToggled2 ? '0' : '1', transform: isToggled2 ? 'scale(0.9)' : 'scale(1)' }}>
+                  <Masonry breakpointCols={breakpointColumnsObj}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column"> 
+                    <motion.div ref={ref} animate={isDesktop ? controls : undefined} variants={isDesktop ? variants : undefined} initial={isDesktop ? "hidden" : undefined} exit={isDesktop ? "exit" : undefined} custom={0}>
+                      <img src='https://api.deandivizio.com/wp-content/uploads/2024/02/IMG_6876-Large.jpeg' alt='An filmic modern portrait of a man in his late 20s' />
+                    </motion.div>
+                    <motion.div animate={isDesktop ? controls : undefined} variants={isDesktop ? variants : undefined} initial={isDesktop ? "hidden" : undefined} exit={isDesktop ? "exit" : undefined} custom={0.33}>
+                      <img src='https://api.deandivizio.com/wp-content/uploads/2024/04/justinPortrait-cropped.jpg ' alt='An stylized portrait of a man in his mid 20s' />
+                    </motion.div>
+                    <motion.div animate={isDesktop ? controls : undefined} variants={isDesktop ? variants : undefined} initial={isDesktop ? "hidden" : undefined} exit={isDesktop ? "exit" : undefined} custom={0.66}>
+                      <img src='https://api.deandivizio.com/wp-content/uploads/2024/02/IMG_6596-Edit-Large.jpeg' alt='An filmic modern portrait of a woman in her early 20s' />
+                    </motion.div>
+                    <motion.div animate={isDesktop ? controls : undefined} variants={isDesktop ? variants : undefined} initial={isDesktop ? "hidden" : undefined} exit={isDesktop ? "exit" : undefined} custom={1}>
+                      <img src='https://api.deandivizio.com/wp-content/uploads/2024/02/DSC03521-scaled.jpg' alt='An filmic professional portrait of a woman in her late 20s' />
+                    </motion.div>
+                  </Masonry>
+                </div>}
+              {isToggled3 &&
+                <div className={styles.videoSection} style={{ opacity: isToggled3 ? '1' : '0', transform: isToggled3 ? 'scale(1)' : 'scale(0.9)' }}>
+                  <iframe
+                    src="https://www.youtube.com/embed/qfta1fxUI7Q?si=iCKkEaUoYEOXeUW4?vq=1080"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen>
+                  </iframe>
+                </div>}
             </div>
-            <h2>Pro</h2>
-          </motion.div>
-          <div className={styles.mediaContainer}>
-            {!isToggled3 &&
-              <div className={styles.photoSection} style={{ opacity: isToggled2 ? '0' : '1', transform: isToggled2 ? 'scale(0.9)' : 'scale(1)' }}>
-                <Masonry breakpointCols={breakpointColumnsObj}
-                  className="my-masonry-grid"
-                  columnClassName="my-masonry-grid_column">
-                  <motion.div ref={ref} animate={controls} variants={variants} initial="hidden" exit="exit" custom={0}>
-                    <img src='https://api.deandivizio.com/wp-content/uploads/2024/02/IMG_6876-Large.jpeg' alt='An filmic modern portrait of a man in his late 20s' />
-                  </motion.div>
-                  <motion.div animate={controls} variants={variants} initial="hidden" exit="exit" custom={0.33}>
-                    <img src='https://api.deandivizio.com/wp-content/uploads/2024/04/justinPortrait-cropped.jpg ' alt='An stylized portrait of a man in his mid 20s' />
-                  </motion.div>
-                  <motion.div animate={controls} variants={variants} initial="hidden" exit="exit" custom={0.66}>
-                    <img src='https://api.deandivizio.com/wp-content/uploads/2024/02/IMG_6596-Edit-Large.jpeg' alt='An filmic modern portrait of a woman in her early 20s' />
-                  </motion.div>
-                  <motion.div animate={controls} variants={variants} initial="hidden" exit="exit" custom={1}>
-                    <img src='https://api.deandivizio.com/wp-content/uploads/2024/02/DSC03521-scaled.jpg' alt='An filmic professional portrait of a woman in her late 20s' />
-                  </motion.div>
-                </Masonry>
-              </div>}
-            {isToggled3 &&
-              <div className={styles.videoSection} style={{ opacity: isToggled3 ? '1' : '0', transform: isToggled3 ? 'scale(1)' : 'scale(0.9)' }}>
-                <iframe
-                  src="https://www.youtube.com/embed/qfta1fxUI7Q?si=iCKkEaUoYEOXeUW4?vq=1080"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowfullscreen>
-                </iframe>
-              </div>}
-          </div>
           </div>
           <motion.h2 id="loveText" animate={skillsControls} variants={fromBottomVariants} initial="hidden" custom={0} >I <span style={loveStyle} className={styles.heroTextColor}><em>Love</em></span> Learning...</motion.h2> {/* words are separated out to allow the word "Love" to fade to a gradient */}
           <div className={styles.skillCardContainer} ref={skillsRef} >
